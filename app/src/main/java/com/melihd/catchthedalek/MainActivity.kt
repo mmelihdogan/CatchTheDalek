11623257e1a50bd67b77d9606ec1e9d56247e687
mmelihdogan <doganmmelih@gmail.com>
2021-05-10 11:13:28 +0300

    added score function

--- app/src/main/java/com/melihd/catchthedalek/MainActivity.kt
+++ app/src/main/java/com/melihd/catchthedalek/MainActivity.kt
@@ -2,12 +2,22 @@ package com.melihd.catchthedalek
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var score = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         println("First commit!")
+    }
+
+    fun increaseScore(view : View) {
+        score++
+        scoreText.text = "Score: ${score}"
     }
 }
