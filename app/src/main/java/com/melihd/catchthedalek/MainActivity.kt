d7ff0b2b57c4351ac1ffc02ffc9d045490c08120
mmelihdogan <doganmmelih@gmail.com>
2021-05-10 11:34:15 +0300

    created runnable function

--- app/src/main/java/com/melihd/catchthedalek/MainActivity.kt
+++ app/src/main/java/com/melihd/catchthedalek/MainActivity.kt
@@ -2,21 +2,60 @@ package com.melihd.catchthedalek
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
+import android.widget.ImageView
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     var score = 0
+    var imageArray = ArrayList<ImageView>()
+    var handler = Handler()
+    var runnable = Runnable {  }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        imageArray.add(imageView1)
+        imageArray.add(imageView2)
+        imageArray.add(imageView3)
+        imageArray.add(imageView4)
+        imageArray.add(imageView5)
+        imageArray.add(imageView6)
+        imageArray.add(imageView7)
+        imageArray.add(imageView8)
+        imageArray.add(imageView9)
+
+        hideImages()
 
     }
 
     fun increaseScore(view : View) {
         score++
         scoreText.text = "Score: ${score}"
+    }
+
+    fun hideImages() {
+
+        runnable = object : Runnable {
+            override fun run() {
+                for (image in imageArray) {
+                    image.visibility = View.INVISIBLE
+                }
+
+                val random = Random()
+                val randomIndex = random.nextInt(9)
+                imageArray[randomIndex].visibility = View.VISIBLE
+
+                handler.postDelayed(runnable,500)
+            }
+        }
+
+        handler.post(runnable)
+
+
     }
 }
