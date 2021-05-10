70a6966c7d51909dbc6320fd230a4de658bafe33
mmelihdogan <doganmmelih@gmail.com>
2021-05-10 09:49:35 +0300

    first commit

---
+++ app/src/main/java/com/melihd/catchthedalek/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.melihd.catchthedalek
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        println("First commit!")
+    }
+}
